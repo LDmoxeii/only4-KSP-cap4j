9b9ded6bcfd44a2178037fdc11976fd476743a34
LD_moxeii <2649075705@qq.com>
2024-11-16 22:29:50 +0800

    暂存

---
+++ src/test/java/com/only4/test/MyService.java
@@ -0,0 +1,7 @@
+package com.only4.test;
+
+public class MyService {
+    public void execute() {
+        StaticUtils.doSomething();
+    }
+}
---
+++ src/test/java/com/only4/test/StaticUtils.java
@@ -0,0 +1,10 @@
+package com.only4.test;
+
+public class StaticUtils {
+    public static void doSomething() {
+        // 静态方法逻辑
+        throw new RuntimeException("Static method called");
+    }
+}
+
+
