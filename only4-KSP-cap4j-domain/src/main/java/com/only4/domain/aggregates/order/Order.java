d15cc05ed8a0d49b11c9b0cbfe3459ae5d6323bc
LD_moxeii <15976781430@163.com>
2024-12-10 11:57:15 +0800

    update 更新为多模块模式

---
+++ only4-KSP-cap4j-domain/src/main/java/com/only4/domain/aggregates/order/Order.java
@@ -0,0 +1,154 @@
+package com.only4.domain.aggregates.order;
+
+import com.only4.domain.aggregates.order.enums.OrderState;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.*;
+import org.netcorepal.cap4j.ddd.domain.aggregate.annotation.Aggregate;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 订单
+ *
+ * 本文件由[cap4j-ddd-codegen-maven-plugin]生成
+ * 警告：请勿手工修改该文件的字段声明，重新生成会覆盖字段声明
+ * @author cap4j-ddd-codegen
+ * @date 2024/11/22
+ */
+@Aggregate(aggregate = "Order", name = "Order", root = true, type = Aggregate.TYPE_ENTITY, description = "订单")
+@Entity
+@Table(name = "`order`")
+@DynamicInsert
+@DynamicUpdate
+@SQLDelete(sql = "update `order` set `del_flag` = 1 where `id` = ? ")
+@Where(clause = "`del_flag` = 0")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Order {
+
+    // 【行为方法开始】
+
+    public void create() {
+
+    }
+
+    public void refund() {
+        this.state = OrderState.REFUND;
+    }
+
+    public void pay() {
+        if (this.state == OrderState.INIT) {
+            this.payTime = LocalDateTime.now();
+        }
+    }
+
+    public void cancel() {
+        this.state = OrderState.CANCELED;
+
+    }
+
+    // 【行为方法结束】
+
+
+
+    // 【字段映射开始】本段落由[cap4j-ddd-codegen-maven-plugin]维护，请不要手工改动
+
+    /**
+     * ID
+     * bigint
+     */
+    @Id
+    @GeneratedValue(generator = "org.netcorepal.cap4j.ddd.domain.distributed.SnowflakeIdentifierGenerator")
+    @GenericGenerator(name = "org.netcorepal.cap4j.ddd.domain.distributed.SnowflakeIdentifierGenerator", strategy = "org.netcorepal.cap4j.ddd.domain.distributed.SnowflakeIdentifierGenerator")
+    @Column(name = "`id`")
+    Long id;
+
+    /**
+     * 流水号
+     * varchar(255)
+     */
+    @Column(name = "`serial`")
+    String serial;
+
+    /**
+     * 消费者ID
+     * bigint
+     */
+    @Column(name = "`customer_id`")
+    Long customerId;
+
+    /**
+     * 名称
+     * varchar(255)
+     */
+    @Column(name = "`name`")
+    String name;
+
+    /**
+     * 类型
+     * 0:INIT:INIT
+     * int
+     */
+    @Convert(converter = com.only4.domain.aggregates.order.enums.OrderType.Converter.class)
+    @Column(name = "`type`")
+    com.only4.domain.aggregates.order.enums.OrderType type;
+
+    /**
+     * 订单状态
+     * 0:INIT:INIT
+     * 1:REFUND:REFUND
+     * 2:CANCELED:CANCELED
+     * int
+     */
+    @Convert(converter = OrderState.Converter.class)
+    @Column(name = "`state`")
+    OrderState state;
+
+    /**
+     * 总价格
+     * bigint
+     */
+    @Column(name = "`price`")
+    Long price;
+
+    /**
+     * 实付价格
+     * bigint
+     */
+    @Column(name = "`actual_price`")
+    Long actualPrice;
+
+    /**
+     * 支付时间
+     * timestamp
+     */
+    @Column(name = "`pay_time`")
+    LocalDateTime payTime;
+
+    /**
+     * 支付标识
+     * tinyint(1)
+     */
+    @Column(name = "`is_paid`")
+    Boolean isPaid;
+
+    /**
+     * 逻辑删除
+     * tinyint(1)
+     */
+    @Column(name = "`del_flag`")
+    Boolean delFlag;
+
+    // 【字段映射结束】本段落由[cap4j-ddd-codegen-maven-plugin]维护，请不要手工改动
+}
+
+
---
+++ only4-KSP-cap4j-domain/src/main/java/com/only4/domain/aggregates/role/meta/RoleSchema.java
@@ -0,0 +1,156 @@
+package com.only4.domain.aggregates.role.meta;
+
+import com.only4.domain._share.meta.Schema;
+import com.only4.domain.aggregates.role.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+/**
+ * 角色表
+ * 本文件由[cap4j-ddd-codegen-maven-plugin]生成
+ * 警告：请勿手工修改该文件，重新生成会覆盖该文件
+ * @author cap4j-ddd-codegen
+ * @date 2024/12/04
+ */
+@RequiredArgsConstructor
+public class RoleSchema {
+    private final Path<Role> root;
+    private final CriteriaBuilder criteriaBuilder;
+
+    public CriteriaBuilder criteriaBuilder() {
+        return criteriaBuilder;
+    }
+
+    /**
+     * ID
+     * bigint
+     */
+    public Schema.Field<Long> id() {
+        return root == null ? new Schema.Field<>("id") : new Schema.Field<>(root.get("id"));
+    }
+
+    /**
+     * 角色名
+     * varchar(255)
+     */
+    public Schema.Field<String> name() {
+        return root == null ? new Schema.Field<>("name") : new Schema.Field<>(root.get("name"));
+    }
+
+    /**
+     * 角色描述
+     * varchar(255)
+     */
+    public Schema.Field<String> description() {
+        return root == null ? new Schema.Field<>("description") : new Schema.Field<>(root.get("description"));
+    }
+
+    /**
+     * 创建时间
+     * timestamp
+     */
+    public Schema.Field<java.time.LocalDateTime> createdAt() {
+        return root == null ? new Schema.Field<>("createdAt") : new Schema.Field<>(root.get("createdAt"));
+    }
+
+
+    /**
+     * 满足所有条件
+     * @param restrictions
+     * @return
+     */
+    public Predicate all(Predicate... restrictions) {
+        return criteriaBuilder().and(restrictions);
+    }
+
+    /**
+     * 满足任一条件
+     * @param restrictions
+     * @return
+     */
+    public Predicate any(Predicate... restrictions) {
+        return criteriaBuilder().or(restrictions);
+    }
+
+    /**
+     * 指定条件
+     * @param builder
+     * @return
+     */
+    public Predicate spec(Schema.PredicateBuilder<RoleSchema> builder){
+        return builder.build(this);
+    }
+
+    /**
+     * RolePermission 关联查询条件定义
+     *
+     * @param joinType
+     * @return
+     */
+    public RolePermissionSchema joinRolePermission(Schema.JoinType joinType) {
+        JoinType type = joinType.toJpaJoinType();
+        Join<Role, com.only4.domain.aggregates.role.RolePermission> join = ((Root<Role>) root).join("rolePermissions", type);
+        RolePermissionSchema schema = new RolePermissionSchema(join, criteriaBuilder);
+        return schema;
+    }
+
+    /**
+     * 构建查询条件
+     * @param builder
+     * @param distinct
+     * @return
+     */
+    public static Specification<Role> specify(Schema.PredicateBuilder<RoleSchema> builder, boolean distinct) {
+        return (root, criteriaQuery, criteriaBuilder) -> {
+            RoleSchema role = new RoleSchema(root, criteriaBuilder);
+            criteriaQuery.where(builder.build(role));
+            criteriaQuery.distinct(distinct);
+            return null;
+        };
+    }
+
+    /**
+     * 构建查询条件
+     * @param builder
+     * @return
+     */
+    public static Specification<Role> specify(Schema.PredicateBuilder<RoleSchema> builder) {
+        return (root, criteriaQuery, criteriaBuilder) -> {
+            RoleSchema role = new RoleSchema(root, criteriaBuilder);
+            criteriaQuery.where(builder.build(role));
+            return null;
+        };
+    }
+
+    /**
+     * 构建排序
+     * @param builders
+     * @return
+     */
+    public static Sort orderBy(Schema.OrderBuilder<RoleSchema>... builders) {
+        return orderBy(Arrays.asList(builders));
+    }
+
+    /**
+     * 构建排序
+     *
+     * @param builders
+     * @return
+     */
+    public static Sort orderBy(Collection<Schema.OrderBuilder<RoleSchema>> builders) {
+        if(null == builders || builders.isEmpty()) {
+            return Sort.unsorted();
+        }
+        return Sort.by(builders.stream()
+                .map(builder -> builder.build(new RoleSchema(null, null)))
+                .collect(Collectors.toList())
+        );
+    }
+
+}
