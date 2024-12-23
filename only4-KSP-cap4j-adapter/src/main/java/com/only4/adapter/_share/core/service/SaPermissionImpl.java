2737b892a9eeff99d45fddf76ab1695fb5645da2
LD_moxeii <15976781430@163.com>
2024-12-23 16:28:39 +0800

    修改权限管理方面细节

--- only4-KSP-cap4j-adapter/src/main/java/com/only4/adapter/_share/core/service/SaPermissionImpl.java
+++ only4-KSP-cap4j-adapter/src/main/java/com/only4/adapter/_share/core/service/SaPermissionImpl.java
@@ -1,8 +1,15 @@
 package com.only4.adapter._share.core.service;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.only4.adapter._share.utils.LoginHelper;
+import com.only4.domain.aggregates.admin_user.AdminUser;
+import com.only4.domain.aggregates.admin_user.AdminUserPermission;
+import lombok.val;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * sa-token 权限管理实现类
@@ -24,6 +31,9 @@ public class SaPermissionImpl implements StpInterface {
      */
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
-      return new ArrayList<>();
+        val loginUser = LoginHelper.getLoginUser(AdminUser.class);
+        return Objects.requireNonNull(loginUser).getAdminUserPermissions().stream()
+                .map(AdminUserPermission::getPermissionCode)
+                .collect(Collectors.toList());
     }
 }
--- only4-KSP-cap4j-adapter/src/main/java/com/only4/adapter/_share/utils/LoginHelper.java
+++ only4-KSP-cap4j-adapter/src/main/java/com/only4/adapter/_share/utils/LoginHelper.java
@@ -1,18 +1,17 @@
 package com.only4.adapter._share.utils;
 
-import static com.only4.adapter._share.constants.MemberConstant.CUSTOMER_ACCOUNT_KEY;
-import static com.only4.adapter._share.constants.MemberConstant.CUSTOMER_KEY;
-import static com.only4.adapter._share.constants.MemberConstant.LOGIN_USER_KEY;
-
 import cn.dev33.satoken.session.SaSession;
 import cn.dev33.satoken.stp.SaLoginModel;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.ObjectUtil;
+import com.only4.domain.aggregates.admin_user.AdminUser;
 import com.only4.domain.aggregates.member.Member;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import static com.only4.adapter._share.constants.UserConstant.*;
 
 /**
  * @author LD_moxeii
@@ -21,77 +20,90 @@ import lombok.extern.slf4j.Slf4j;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LoginHelper {
 
-  public static void login(Member loginUser) {
-    login(loginUser, null);
-  }
-
-  public static void login(Member loginUser, SaLoginModel model) {
-    model = ObjectUtil.defaultIfNull(model, new SaLoginModel());
-    model.setExtra(CUSTOMER_KEY, loginUser.getId());
-    StpUtil.login(loginUser.getId(), model);
-    StpUtil.getSession().set(LOGIN_USER_KEY, loginUser);
-  }
-
-  /**
-   * 获取用户(多级缓存)
-   */
-  public static Member getLoginUser() {
-    SaSession session = StpUtil.getTokenSession();
-    if (ObjectUtil.isNull(session)) {
-      return null;
+    public static <T> void login(T loginUser) {
+        login(loginUser, null);
     }
-    return (Member) session.get(LOGIN_USER_KEY);
-  }
 
-  /**
-   * 获取用户基于token
-   */
-  public static Member getLoginUser(String token) {
-    SaSession session = StpUtil.getTokenSessionByToken(token);
-    if (ObjectUtil.isNull(session)) {
-      return null;
+    public static <T> void login(T loginUser, SaLoginModel model) {
+        model = ObjectUtil.defaultIfNull(model, new SaLoginModel());
+        if (loginUser instanceof Member) {
+            Member member = (Member)loginUser;
+            model.setExtra(CUSTOMER_KEY, member.getId());
+            StpUtil.login(member.getId(), model);
+            StpUtil.getSession().set(LOGIN_USER_KEY, member);
+        } else if (loginUser instanceof AdminUser) {
+            AdminUser adminUser = (AdminUser)loginUser;
+            model.setExtra(CUSTOMER_KEY, adminUser.getId());
+            StpUtil.login(adminUser.getId(), model);
+            StpUtil.getSession().set(LOGIN_USER_KEY, adminUser);
+        }
+
     }
-    return (Member) session.get(LOGIN_USER_KEY);
-  }
 
-  /**
-   * 获取用户id
-   */
-  public static Long getMemberId() {
-    return Convert.toLong(getExtra(CUSTOMER_KEY));
-  }
+    /**
+     * 获取用户(多级缓存)
+     */
+    public static <T> T getLoginUser(Class<T> clazz) {
+        SaSession session = StpUtil.getTokenSession();
+        if (ObjectUtil.isNull(session)) {
+            return null;
+        }
 
-  /**
-   * 获取用户账户
-   */
-  public static String getAccount() {
-    return Convert.toStr(getExtra(CUSTOMER_ACCOUNT_KEY));
-  }
-
-  /**
-   * 获取当前 Token 的扩展信息
-   *
-   * @param key 键值
-   * @return 对应的扩展数据
-   */
-  private static Object getExtra(String key) {
-    try {
-      return StpUtil.getExtra(key);
-    } catch (Exception e) {
-      return null;
+        Object user = session.get(LOGIN_USER_KEY);
+        // 检查类型安全
+        if (!clazz.isInstance(user)) {
+            throw new ClassCastException("Expected type " + clazz.getName() + ", but got " + user.getClass().getName());
+        }
+        return clazz.cast(user);
     }
-  }
 
-  /**
-   * 检查当前用户是否已登录
-   *
-   * @return 结果
-   */
-  public static boolean isLogin() {
-    try {
-      return getLoginUser() != null;
-    } catch (Exception e) {
-      return false;
+    /**
+     * 获取用户基于token
+     */
+    public static <T> T getLoginUser(Class<T> clazz, String token) {
+        SaSession session = StpUtil.getTokenSessionByToken(token);
+        if (ObjectUtil.isNull(session)) {
+            return null;
+        }
+        Object user = session.get(LOGIN_USER_KEY);
+        // 检查类型安全
+        if (!clazz.isInstance(user)) {
+            throw new ClassCastException("Expected type " + clazz.getName() + ", but got " + user.getClass().getName());
+        }
+        return clazz.cast(user);
     }
-  }
+
+    /**
+     * 获取用户id
+     */
+    public static Long getUserId() {
+        return Convert.toLong(getExtra(CUSTOMER_KEY));
+    }
+
+    /**
+     * 获取当前 Token 的扩展信息
+     *
+     * @param key 键值
+     * @return 对应的扩展数据
+     */
+    private static Object getExtra(String key) {
+        try {
+            return StpUtil.getExtra(key);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 检查当前用户是否已登录
+     *
+     * @return 结果
+     */
+    public static <T> boolean isLogin(Class<T> clazz) {
+        try {
+            return getLoginUser(clazz) != null;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
