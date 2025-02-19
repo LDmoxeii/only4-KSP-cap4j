7d56d351c0e6ea5c4c03fb2fd96728c0f85f5946
LD_moxeii <15976781430@163.com>
2025-02-19 19:23:45 +0800

    feat(article_comment): 实现文章评论相关命令
    - 新增创建、删除、举报、更新评论信息、更新点赞数、更新回复数、更新置顶状态等命令
    - 优化 ArticleComment 类，支持新功能
    - 添加领域事件，如评论创建、删除、点赞数更新等

---
+++ only4-KSP-cap4j-application/src/main/java/com/only4/application/commands/article_comment/UpdateArticleCommentInfoCmd.java
@@ -0,0 +1,75 @@
+package com.only4.application.commands.article_comment;
+
+
+import com.only4._share.exception.KnownException;
+import com.only4.application.validater.MemberExists;
+import com.only4.domain.aggregates.article_comment.ArticleComment;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.netcorepal.cap4j.ddd.Mediator;
+import org.netcorepal.cap4j.ddd.application.RequestParam;
+import org.netcorepal.cap4j.ddd.application.command.Command;
+import org.netcorepal.cap4j.ddd.domain.repo.JpaPredicate;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * 更新文章评论信息
+ *
+ * @author cap4j-ddd-codegen
+ * @date 2025/02/19
+ */
+public class UpdateArticleCommentInfoCmd {
+
+    /**
+     * UpdateArticleCommentInfoCmd命令请求实现
+     */
+    @Service
+    @RequiredArgsConstructor
+    @Slf4j
+    public static class Handler implements Command<Request, Response> {
+        @Override
+        public Response exec(Request cmd) {
+            ArticleComment comment = Mediator.repositories()
+                    .findOne(JpaPredicate.byId(ArticleComment.class, cmd.getCommentId()))
+                    .orElseThrow(() -> new KnownException("评论不存在"));
+
+            comment.updateInfo(cmd.getMemberId(), cmd.getMemberName());
+            Mediator.uow().persist(comment);
+            Mediator.uow().save();
+
+            return Response.builder()
+                    .success(true)
+                    .build();
+        }
+    }
+
+    /**
+     * UpdateArticleCommentInfoCmd命令请求参数
+     */
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Request implements RequestParam<Response> {
+        Long commentId;
+
+        @MemberExists
+        Long memberId;
+
+        @NotEmpty
+        String memberName;
+    }
+
+    /**
+     * UpdateArticleCommentInfoCmd命令响应
+     */
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        boolean success;
+    }
+}
