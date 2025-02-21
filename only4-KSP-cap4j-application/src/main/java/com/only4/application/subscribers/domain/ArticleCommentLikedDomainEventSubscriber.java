0c4b85264aeaa0dc9210a269c00bd5118393e7cd
LD_moxeii <15976781430@163.com>
2025-02-21 14:25:46 +0800

    feat(application): 增加文章评论点赞和取消点赞的功能
    - 在 ArticleCommentLikedDomainEventSubscriber 和 ArticleCommentUnlikedDomainEventSubscriber 中添加逻辑处理
    - 使用 UpdateArticleCommentLikeCountCmd 命令更新评论的点赞数
    -引入 Mediator 以支持命令的发送

--- only4-KSP-cap4j-application/src/main/java/com/only4/application/subscribers/domain/ArticleCommentLikedDomainEventSubscriber.java
+++ only4-KSP-cap4j-application/src/main/java/com/only4/application/subscribers/domain/ArticleCommentLikedDomainEventSubscriber.java
@@ -1,9 +1,14 @@
 package com.only4.application.subscribers.domain;
 
+import com.only4.application.commands.article_comment.UpdateArticleCommentLikeCountCmd;
+import com.only4.domain.aggregates.article_comment_like.ArticleCommentLike;
 import com.only4.domain.aggregates.article_comment_like.events.ArticleCommentLikedDomainEvent;
 import lombok.RequiredArgsConstructor;
+import org.netcorepal.cap4j.ddd.Mediator;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * ArticleCommentLike.ArticleCommentLikedDomainEvent领域事件订阅
@@ -14,8 +19,14 @@ import org.springframework.stereotype.Service;
 public class ArticleCommentLikedDomainEventSubscriber {
 
     @EventListener(ArticleCommentLikedDomainEvent.class)
-    public void on(ArticleCommentLikedDomainEvent event) {
-        
+    public void updateArticleCommentLikeCount(ArticleCommentLikedDomainEvent event) {
+        ArticleCommentLike record = event.getEntity();
+
+        Optional.of(UpdateArticleCommentLikeCountCmd.Request.builder()
+                        .commentId(record.getArticleCommentId())
+                        .likeCount(1)
+                        .build())
+                .ifPresent(Mediator.commands()::send);
     }
 
 }
--- only4-KSP-cap4j-application/src/main/java/com/only4/application/subscribers/domain/ArticleCommentUnlikedDomainEventSubscriber.java
+++ only4-KSP-cap4j-application/src/main/java/com/only4/application/subscribers/domain/ArticleCommentUnlikedDomainEventSubscriber.java
@@ -1,9 +1,14 @@
 package com.only4.application.subscribers.domain;
 
+import com.only4.application.commands.article_comment.UpdateArticleCommentLikeCountCmd;
+import com.only4.domain.aggregates.article_comment_like.ArticleCommentLike;
 import com.only4.domain.aggregates.article_comment_like.events.ArticleCommentUnlikedDomainEvent;
 import lombok.RequiredArgsConstructor;
+import org.netcorepal.cap4j.ddd.Mediator;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * ArticleCommentLike.ArticleCommentUnlikedDomainEvent领域事件订阅
@@ -14,8 +19,14 @@ import org.springframework.stereotype.Service;
 public class ArticleCommentUnlikedDomainEventSubscriber {
 
     @EventListener(ArticleCommentUnlikedDomainEvent.class)
-    public void on(ArticleCommentUnlikedDomainEvent event) {
-        
+    public void updateArticleCommentLikeCount(ArticleCommentUnlikedDomainEvent event) {
+        ArticleCommentLike record = event.getEntity();
+
+        Optional.of(UpdateArticleCommentLikeCountCmd.Request.builder()
+                        .commentId(record.getArticleCommentId())
+                        .likeCount(-1)
+                        .build())
+                .ifPresent(Mediator.commands()::send);
     }
 
 }
