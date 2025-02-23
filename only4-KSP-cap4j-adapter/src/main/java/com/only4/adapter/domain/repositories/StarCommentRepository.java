85c8ac69d51a471fc7b7fada7455882be12428f6
LD_moxeii <15976781430@163.com>
2025-02-23 17:36:21 +0800

    feat(domain): 重构星球评论领域模型并添加新功能 #127 #128 #129 #133 #132 #131 #130
    -重新定义了 StarComment 领域对象的属性和行为
    - 新增了多个与星球评论相关的命令和事件
    - 实现了置顶评论、更新评论信息等功能
    - 优化了评论点赞、回复和举报的处理逻辑
    - 调整了领域事件的发布和订阅机制

---
+++ only4-KSP-cap4j-adapter/src/main/java/com/only4/adapter/domain/repositories/StarCommentRepository.java
@@ -0,0 +1,26 @@
+package com.only4.adapter.domain.repositories;
+
+import com.only4.domain.aggregates.star_comment.StarComment;
+import org.netcorepal.cap4j.ddd.domain.aggregate.annotation.Aggregate;
+import org.netcorepal.cap4j.ddd.domain.repo.AbstractJpaRepository;
+import org.netcorepal.cap4j.ddd.domain.repo.AggregateRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Component;
+/**
+ * 本文件由[cap4j-ddd-codegen-maven-plugin]生成
+ * @author cap4j-ddd-codegen
+ * @date 2025/02/23
+ */
+public interface StarCommentRepository extends AggregateRepository<StarComment, Long> {
+
+    @Component
+    @Aggregate(aggregate = "StarComment", name = "StarComment", type = Aggregate.TYPE_REPOSITORY, description = "")
+    public static class StarCommentJpaRepositoryAdapter extends AbstractJpaRepository<StarComment, Long>
+    {
+        public StarCommentJpaRepositoryAdapter(JpaSpecificationExecutor<StarComment> jpaSpecificationExecutor, JpaRepository<StarComment, Long> jpaRepository) {
+            super(jpaSpecificationExecutor, jpaRepository);
+        }
+    }
+
+}
