4613565e4dd02fee746d151543c6ff25ef0221b7
xijure <3193064944@qq.com>
2025-03-09 22:44:57 +0800

    Update StarCommentLike DomainEventSubscriber

--- only4-KSP-cap4j-application/src/main/java/com/only4/application/subscribers/domain/StarCommentLikedDomainEventSubscriber.java
+++ only4-KSP-cap4j-application/src/main/java/com/only4/application/subscribers/domain/StarCommentLikedDomainEventSubscriber.java
@@ -1,9 +1,14 @@
 package com.only4.application.subscribers.domain;
 
+import com.only4.application.commands.star.UpdateStarLikeCountCmd;
+import com.only4.domain.aggregates.star_comment_like.StarCommentLike;
 import com.only4.domain.aggregates.star_comment_like.events.StarCommentLikedDomainEvent;
 import lombok.RequiredArgsConstructor;
+import org.netcorepal.cap4j.ddd.Mediator;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * StarCommentLike.StarCommentLikedDomainEvent领域事件订阅
@@ -14,8 +19,13 @@ import org.springframework.stereotype.Service;
 public class StarCommentLikedDomainEventSubscriber {
 
     @EventListener(StarCommentLikedDomainEvent.class)
-    public void on(StarCommentLikedDomainEvent event) {
-        
+    public void updateStarLikeCount(StarCommentLikedDomainEvent event) {
+        StarCommentLike record = event.getEntity();
+
+        Optional.of(UpdateStarLikeCountCmd.Request.builder()
+                .starId(record.getStarId())
+                .likeCount(1)
+                .build()).ifPresent(Mediator.commands()::send);
     }
 
 }
